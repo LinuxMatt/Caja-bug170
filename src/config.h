1a9da8bbe61203d04515a2640ea6de0962b39734
Matt Thirtytwo <matt.59491@gmail.com>
2013-12-30 20:49:45 +0100

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,121 @@
+#define ENABLE_NLS 1
+#define ENABLE_PACKAGEKIT 1
+#define GETTEXT_PACKAGE "caja"
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+#define HAVE_BIND_TEXTDOMAIN_CODESET 1
+
+/* Define to 1 if you have the `dcgettext' function. */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to enable xmp support */
+#define HAVE_EXEMPI 1
+
+/* Define if we have exempi with the new API */
+#define HAVE_EXEMPI_NEW_API 1
+
+/* Define to enable EXIF support */
+#define HAVE_EXIF 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* needed for egg-screen-exec functions */
+#define HAVE_GTK_MULTIHEAD /**/
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+#define HAVE_LC_MESSAGES 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the <locale.h> header file. */
+#define HAVE_LOCALE_H 1
+
+/* Define to 1 if you have the <malloc.h> header file. */
+#define HAVE_MALLOC_H 1
+
+/* Define to 1 if you have the `mallopt' function. */
+#define HAVE_MALLOPT 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* */
+#define HAVE_RENDER 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if strftime supports %E and %O modifiers. */
+#define HAVE_STRFTIME_EXTENSION 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/mount.h> header file. */
+#define HAVE_SYS_MOUNT_H 1
+
+/* Define to 1 if you have the <sys/param.h> header file. */
+#define HAVE_SYS_PARAM_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <sys/vfs.h> header file. */
+#define HAVE_SYS_VFS_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <X11/XF86keysym.h> header file. */
+#define HAVE_X11_XF86KEYSYM_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define the location where the catalogs will be installed */
+#define MATELOCALEDIR "/usr/local/share/locale"
+
+/* Name of package */
+#define PACKAGE "caja"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "http://www.mate-desktop.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "caja"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "caja 1.7.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "caja"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.7.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.7.0"
